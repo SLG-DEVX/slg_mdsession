ce67e73ab7333411f501ce9f4f3a69f246625286
GEEKMDXINC <herukobot67@gmail.com>
2025-02-05 01:41:51 +0100

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,11 @@
-function makeid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+function makeid() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < 10; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
   }
-  return result;
+  return result ;
 }
+
+
 module.exports = {makeid};
